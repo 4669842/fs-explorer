208d43938891b048721d575446693116abaec028
Kyle Robinson Young <kyle@dontkry.com>
2016-03-09 19:53:06 -0800

    Lots of updates

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-var $ = require('bel')
+var bel = require('bel')
+var csjs = require('csjs')
+var update = require('./lib/update')
 var viewer = require('./lib/viewer')
 var tree = require('./lib/tree')
 
 module.exports = function explorer (files) {
   console.time('explorer')
   function render (selected) {
-    return $`<div className="fs-explorer row">
+    return bel`<div class="${className}">
       ${tree(files, onselected)}
       ${viewer(selected, onselected)}
     </div>`
   }
   function onselected (file) {
-    element.update(render(file))
+    update('.' + className, render(file))
   }
   var element = render(files[0])
   console.timeEnd('explorer')
   return element
 }
+
+var styles = module.exports.styles = csjs`
+.fs-explorer {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+}
+`
+var className = styles['fs-explorer']
